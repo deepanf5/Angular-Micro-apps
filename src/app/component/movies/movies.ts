9ca3cdd547342af5a0108f4383eae58c5bd911c5
deepan <deepanappu1999@gmail.com>
2025-12-28 13:29:00 +0530

    adding the window events

--- src/app/component/movies/movies.ts
+++ src/app/component/movies/movies.ts
@@ -9,58 +9,59 @@ import { Component } from '@angular/core';
 })
 export class Movies {
 
-  movie:any
+  movie: any
 
   moviesList = [
 
     {
-      id:1,
-      title:'movie 1',
-      poster:'poster.jpg'
-    },
-     {
-      id:2,
-      title:'movie 2',
-      poster:'poster.jpg'
-    },
-     {
-      id:3,
-      title:'movie 3',
-      poster:'poster.jpg'
-    },
-     {
-      id:4,
-      title:'movie 4',
-      poster:'poster.jpg'
+      id: 1,
+      title: 'movie 1',
+      poster: 'poster.jpg'
     },
     {
-      id:5,
-        title:'movie 5',
-      poster:'poster.jpg'
+      id: 2,
+      title: 'movie 2',
+      poster: 'poster.jpg'
     },
     {
-      id:6,
-        title:'movie 6',
-      poster:'poster.jpg'
+      id: 3,
+      title: 'movie 3',
+      poster: 'poster.jpg'
     },
     {
-      id:7,
-        title:'movie 7',
-      poster:'poster.jpg'
+      id: 4,
+      title: 'movie 4',
+      poster: 'poster.jpg'
     },
     {
-      id:8,
-        title:'movie 8',
-      poster:'poster.jpg'
+      id: 5,
+      title: 'movie 5',
+      poster: 'poster.jpg'
+    },
+    {
+      id: 6,
+      title: 'movie 6',
+      poster: 'poster.jpg'
+    },
+    {
+      id: 7,
+      title: 'movie 7',
+      poster: 'poster.jpg'
+    },
+    {
+      id: 8,
+      title: 'movie 8',
+      poster: 'poster.jpg'
     }
   ]
 
 
-  getMovie(Movie:any) {
+  getMovie(Movie: any) {
 
-   this.movie =  this.moviesList.find(movie => Movie.id === movie.id)
-
-   window.dispatchEvent(new CustomEvent('selectedMovie',{detail:Movie}))
+    const event = new CustomEvent('MOVIE_SELECTED', {
+      detail: { movie: Movie }
+    });
+    window.dispatchEvent(event);
 
   }
 
